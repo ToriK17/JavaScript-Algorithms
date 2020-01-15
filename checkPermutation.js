fa28cdffd3dfe452cb5ee5658161b9b1a49a33ee
ToriK17 <vfcad@protonmail.com>
2020-01-14 16:58:08 -0800

    adds permutation JS algorithm

--- checkPermutation.js
+++ checkPermutation.js
@@ -1,1 +1,20 @@
 // Write a function that checks whether two given strings are Permutations of each other or not. A permutation of a string is another string that contains the same characters, only the order of the characters can be different. example: "abcd" and "dcba" are permutations of each other. 
+
+function checkPermutation(string1, string2) {
+  let arrayOne = string1.split('').sort();
+  let arrayTwo = string2.split('').sort();
+  let check = false; 
+ 
+  for (var i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      check = false;
+    } else {
+      check = true;
+    }
+  }
+  console.log(check);
+}
+
+checkPermutation("apples", "bananas");
+checkPermutation("abcde", "edcba");
+// learned that .sort() works alphabetically
