091c5b5562bb82b58328cbc24af5d954b81fccfa
ToriK17 <vfcad@protonmail.com>
2020-01-16 11:10:55 -0800

    adds oneAway algorithm

---
+++ oneAway.js
@@ -0,0 +1,8 @@
+// There are three types of edits that can be preformed on strings: insert a character, remove a character, or replace a character. 
+// Given two strings, write a function to check if they are one edit 9or zero edits) away. 
+// EXAMPLE
+/*  pale, ple -> true
+    pales, pale -> true
+    pale, bale -> true
+    pale, bake -> false
+*/
