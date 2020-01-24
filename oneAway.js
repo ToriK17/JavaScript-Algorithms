ac3b2a9f718eb131ccc036c9c1c143a5ff419467
ToriK17 <vfcad@protonmail.com>
2020-01-23 17:59:21 -0800

    adds partial zeroMatrix algorithm

--- oneAway.js
+++ oneAway.js
@@ -1,5 +1,5 @@
 // There are three types of edits that can be preformed on strings: insert a character, remove a character, or replace a character. 
-// Given two strings, write a function to check if they are one edit 9or zero edits) away. 
+// Given two strings, write a function to check if they are one edit or zero edits) away. 
 // EXAMPLE
 /*  pale, ple -> true
     pales, pale -> true
@@ -8,15 +8,42 @@
 */
 
 function oneAway(string1, string2) {
-  //variable: differences between the strings
+  let differences = 0;
+  let arrayOne = string1.split('').sort();
+  let arrayTwo = string2.split('').sort();
+
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne.includes(arrayTwo[i])) {
+      differences += 0;     
+    } else {
+      differences += 1;
+    }
+  }
+  if (differences > 1) {
+    console.log('false');
+  } else if ((arrayOne.length !== arrayTwo.length) && differences < 1 ) {
+    console.log('true');
+  } else {
+    console.log('true');
+  }
   
 
-  // compare lengths, if different you're only allowed one different otherwise they must be identical
-
-  // if lengths are the same:
-  // like permutations check each character is the same and if not increase differences count by one
-
-  // then dependent on the differences value return oneAway true or false
+  
 }
 
+
+// if lengths are the same:
+// like permutations check each character is the same and if not increase differences count by one
+
+// then dependent on the differences value return oneAway true or false
+
+// case 1 
+// remove a character case or add a character
+// compare lengths, if different you're only allowed one different otherwise they must be identical
 oneAway('pale', 'ple');
+oneAway('pales', 'pale');
+
+// case 2 
+// same length but one character is different. 
+oneAway('pale', 'bale');
+oneAway('pale', 'bake');
---
+++ zeroMatrix.js
@@ -0,0 +1,37 @@
+// Zero matrix: Write an algorithm such that if an element is an M x N matrix is 0, it's entire row and column are set to 0.
+
+let matrix = [
+  [7,2,6], 
+  [-2,3,4], 
+  [3,0,1], 
+  [2,3,0] 
+  ];
+// console.log(matrix[2].fill(0));
+
+
+function zeroMatrix(matrix) {
+  //first figure out how many rows and columns
+  let columns = matrix[0].length;
+  let rows = matrix.length;
+  let newMatrix = []; 
+  let zeroedColumns = [];
+  
+
+  // check rows
+  for (let i = 0; i < rows; i++) {
+    if (matrix[i].includes(0)) {
+      zeroedColumns.push(matrix[i].indexOf(0));
+      newMatrix.push(matrix[i].fill(0));
+    } else {
+      newMatrix.push(matrix[i]);
+    }
+  }
+  // now zero columns
+
+
+
+  console.log(newMatrix);
+  console.log(zeroedColumns);
+}  
+zeroMatrix(matrix);
+
